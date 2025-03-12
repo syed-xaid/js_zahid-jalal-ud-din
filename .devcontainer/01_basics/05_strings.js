8bb3f12d63253304f396817dcd76e12aa886a4f6
Zahid Jalal ud din <116332444+syed-xaid@users.noreply.github.com>
2025-03-12 09:17:30 +0000

    complete

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const gameName="zahid jalal ud din"
+
+
+
+
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
