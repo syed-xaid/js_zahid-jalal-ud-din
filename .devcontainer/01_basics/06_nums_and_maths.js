09e3c0f3271ac0142066875a055950bf5db7b256
Zahid Jalal ud din <116332444+syed-xaid@users.noreply.github.com>
2025-03-16 08:28:37 +0000

    done

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+// const score=400
+
+// const balance=new Number(100)
+
+
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+// const hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//   ++++++++++++++++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2));
+// console.log(Math.min(2,5,3,6,7,8))
+// console.log(Math.max(4,6,3,2,2,5,9,7));
+
+
+console.log( Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random() * (max-min+1))+min)
---
+++ .devcontainer/01_basics/07_date_and_time.js
@@ -0,0 +1,19 @@
+
+//console.log(new Date(8.64e15).toString()); // "Sat Sep 13 275760 00:00:00 GMT+0000 (Coordinated Universal Time)"
+//console.log(new Date(8.64e15 + 1).toString()); // "Invalid Date"
+
+
+let myDate = new Date()
+
+// console.log(myDate.toString())
+
+
+// console.log(myDate.toDateString())
+
+// console.log(myDate.toTimeString())
+
+let myCreatedDate = new Date(2025, 0, 16)
+
+console.log(myCreatedDate.toDateString());
+
+
